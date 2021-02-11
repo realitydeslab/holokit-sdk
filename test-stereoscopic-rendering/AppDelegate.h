edb7615e4c588445e6934a991bf2245eb6cd7020
Yuchen <yuchen@Nicoles-MacBook-Pro-2.local>
2021-02-11 13:36:30 +0800

    Stereoscopic rendering code written by Objective-C

---
+++ test-stereoscopic-rendering/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  test-stereoscopic-rendering
+//
+//  Created by Yuchen on 2021/2/10.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test-stereoscopic-rendering/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  test-stereoscopic-rendering
+//
+//  Created by Yuchen on 2021/2/10.
+//
+
+#import <UIKit/UIKit.h>
+#import <Metal/Metal.h>
+#import <MetalKit/MetalKit.h>
+#import <ARKit/ARKit.h>
+
+@interface ViewController : UIViewController
+
+@end
+
+
