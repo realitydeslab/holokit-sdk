f081ba9d9a52d4fce067197818cdc99c32100a42
yuchen <yuchen@holoi.com>
2021-07-07 17:33:12 +0800

    Unity default sound effects implemented through the network

--- test-projects/Display/Assets/Scripts/FloatingBall.cs
+++ test-projects/Display/Assets/Scripts/FloatingBall.cs
@@ -2,16 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using MLAPI;
+using MLAPI.Messaging;
 
 public class FloatingBall : NetworkBehaviour
 {
 
     [SerializeField] private float m_HandBouncingFactor = 0.2f;
+
+    private AudioSource m_AudioSource;
+
+    [SerializeField] private AudioClip m_HitPlaneAudioClip;
     
     void Start()
     {
         // (0.00, -9.81, 0.00) as default
         Physics.gravity = new Vector3(0f, -2f, 0f);
+
+        m_AudioSource = GetComponent<AudioSource>();
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -29,8 +36,34 @@ public class FloatingBall : NetworkBehaviour
             //Vector3 direction = (transform.position - handSpherePosition).normalized;
             Vector3 direction = collision.transform.forward;
             GetComponent<Rigidbody>().AddForce(direction * m_HandBouncingFactor);
+            return;
         }
+        if (collision.gameObject.tag.Equals("Plane"))
+        {
+            // Play audio effect
+            if (IsServer)
+            {
+                m_AudioSource.clip = m_HitPlaneAudioClip;
+                m_AudioSource.Play();
+                PlayAudioEffectHitPlaneServerRpc();
+            }
+        }
+    }
 
+    [ServerRpc]
+    private void PlayAudioEffectHitPlaneServerRpc()
+    {
+        PlayAudioEffectHitPlaneClientRpc();
+    }
+
+    [ClientRpc]
+    private void PlayAudioEffectHitPlaneClientRpc()
+    {
+        if (!IsServer)
+        {
+            m_AudioSource.clip = m_HitPlaneAudioClip;
+            m_AudioSource.Play();
+        }
     }
 
 }
