12a345e1f7d4e45aa6a24c96634d95b7ca98c0e8
haosizheng <18861813833@163.com>
2021-07-16 14:56:41 +0800

    add lerp menu script option

--- test-projects/HoloKitHado/Assets/Scripts/HolokitMRMenuMovementController.cs
+++ test-projects/HoloKitHado/Assets/Scripts/HolokitMRMenuMovementController.cs
@@ -10,10 +10,11 @@ public class HolokitMRMenuMovementController : MonoBehaviour
     [SerializeField] private bool isEnterButton;
 
     [Header("Menu Properties")]
-    [SerializeField] private Vector3 offset = new Vector3(0,0,0.5f);
-    [SerializeField] private float maxSpeed = .1f;
-    [SerializeField] private float maxForce = .1f;
-    [SerializeField] private float distanceThreshlod = .1f;
+    [SerializeField] private Vector3 m_offset = new Vector3(0, 0, 0.5f);
+    [SerializeField] private float m_maxSpeed = .1f;
+    [SerializeField] private float m_lerpSpeed = .2f;
+    [SerializeField] private float m_maxForce = .1f;
+    [SerializeField] private float m_distanceThreshlod = .1f;
 
     private Vector3 velocity;
     private Vector3 acceleration;
@@ -36,40 +37,42 @@ public class HolokitMRMenuMovementController : MonoBehaviour
         vfx = GetComponent<VisualEffect>();
     }
 
-    void FixedUpdate()
+    void Update()
     {
         EyeCenter.position = arCamera.position + arCamera.TransformVector(HoloKitSettings.CameraToCenterEyeOffset);
         EyeCenter.rotation = arCamera.rotation;
-        Debug.Log("fuck0");
+
         Vector3 targetPosition;
         if (isEnterButton)
         {
-            targetPosition = EnterMenuTargetCaculate(EyeCenter, offset);
+            targetPosition = EnterMenuTargetCaculate(EyeCenter, m_offset);
         }
         else
         {
-            targetPosition = BackMenuTargetCaculate(EyeCenter, offset);
+            targetPosition = BackMenuTargetCaculate(EyeCenter, m_offset);
         }
-        Debug.Log("fuck1");
-        newPosition = AnimatedMove(transform.position, targetPosition, maxSpeed, maxForce, distanceThreshlod);
-        Debug.Log("fuck2");
+
+        //newPosition = PhysicalAnimation(transform.position, targetPosition, m_maxSpeed, m_maxForce, m_distanceThreshlod);
+        newPosition = LerpAnimation(transform.position, targetPosition, m_lerpSpeed);
+
         transform.position = newPosition;
         transform.LookAt(EyeCenter);
-        Debug.Log("fuck3");
-        Debug.Log(HadoController.Instance.currentAttackRechargePercent);
         vfx.SetFloat("Bullet Load", HadoController.Instance.currentAttackRechargePercent);
-        Debug.Log("fuck4");
-        Debug.Log(HadoController.Instance.currentShieldRechargePercent);
         vfx.SetFloat("Shield Load", HadoController.Instance.currentShieldRechargePercent);
-        Debug.Log("fuck5");
     }
 
     float map(float x, float minIn, float maxIn, float minOut, float maxOut)
     {
-        x = (((x - minIn) / maxIn) * (maxOut - minOut))+ minOut;
+        x = (((x - minIn) / maxIn) * (maxOut - minOut)) + minOut;
         return x;
     }
-    Vector3 AnimatedMove(Vector3 position, Vector3 targetPosition, float maxSpeed, float maxForce, float distanceThreshlod)
+
+    Vector3 LerpAnimation(Vector3 position, Vector3 targetPosition, float lerpSpeed)
+    {
+        position += (targetPosition - position) * Time.deltaTime * lerpSpeed;
+        return position;
+    }
+    Vector3 PhysicalAnimation(Vector3 position, Vector3 targetPosition, float maxSpeed, float maxForce, float distanceThreshlod)
     {
         float maxSpeedMapDistance = .5f; // get maxspeed when distance reach this value
         Vector3 desired = targetPosition - position;
@@ -112,7 +115,7 @@ public class HolokitMRMenuMovementController : MonoBehaviour
             return position;
         }
     }
-    
+
     Vector3 EnterMenuTargetCaculate(Transform holoPosition, Vector3 offset)
     {
         Vector3 headsetForwardDirection = EyeCenter.TransformDirection(0, 0, 1); // get headset forward direction
@@ -131,9 +134,9 @@ public class HolokitMRMenuMovementController : MonoBehaviour
         Vector3 headsetVerticalDirection = EyeCenter.TransformDirection(0, 1, 0); // get headset forward direction
         Vector3 headsetHorizentalDirection = EyeCenter.TransformDirection(1, 0, 0); // get headset forward direction
         Vector3 offsetX = headsetHorizentalDirection * offset.x;
-        Vector3 offsetY = new Vector3(0,1,0) * offset.y;
+        Vector3 offsetY = new Vector3(0, 1, 0) * offset.y;
         Vector3 offsetZ = headsetForwardDirection * offset.z;
-        Vector3 ooooooo = new Vector3(0 , -.3f, headsetForwardDirection.z*.5f);
+        Vector3 ooooooo = new Vector3(0, -.3f, headsetForwardDirection.z * .5f);
         Vector3 sum = offsetX + offsetY + offsetZ;
         return holoPosition.position + ooooooo;
     }
