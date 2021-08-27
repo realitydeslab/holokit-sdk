e2fdd519a26aaab85b9ce59dc1a8b59af30be99e
sizheng <18861813833@163.com>
2021-08-27 10:57:28 +0800

    update card scene

--- test-projects/TheMagic/Assets/Scenes/SingleCard/SingleCardManager.cs
+++ test-projects/TheMagic/Assets/Scenes/SingleCard/SingleCardManager.cs
@@ -1,19 +1,27 @@
+/// this is a card-touching controller
+/// 
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SingleCardManager : MonoBehaviour
 {
-    [SerializeField] float m_Speed =1f;
+    [SerializeField] float m_Speed = 1f;
+    [SerializeField] Vector3 testRotate;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+#if (UNITY_EDITOR)
+        // do your code
+        transform.Rotate(testRotate);
+#endif
+
         if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0); // get first touch since touch count is greater than zero
@@ -24,9 +32,16 @@ public class SingleCardManager : MonoBehaviour
                 Vector3 touchedPos = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 10));
                 // lerp and set the position of the current object to that of the touch, but smoothly over time.
                 //transform.position = Vector3.Lerp(transform.position, touchedPos, Time.deltaTime);
+                var dx = touch.deltaPosition.x;
+                var dy = touch.deltaPosition.y;
 
-                transform.Rotate(new Vector3(touch.deltaPosition.y, touch.deltaPosition.x, 0) * m_Speed);
+                transform.Rotate(new Vector3(-dy, -dx, 0) * m_Speed);
+                Debug.Log(touchedPos);
             }
+        }
+        else
+        {
+            Debug.Log("no touch found");
         }
     }
 
