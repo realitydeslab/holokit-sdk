d7e7a27ed8f74d58333be8f44d5c8d01811f529e
Botao Hu <me@botao.hu>
2022-10-18 16:17:42 -0400

    default arkit metal samples app

---
+++ low-latency-tracking-mockapp/low_latency_tracking_mockapp-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  low_latency_tracking_mockapp-Bridging-Header.h
+//  low-latency-tracking-mockapp
+//
+//  Created by Botao Hu on 10/18/22.
+//
+
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "ShaderTypes.h"
