1f6a4e35acac777ea423102eb86953af16673d98
yuchen <yuchenz27@outlook.com>
2023-01-29 08:49:45 +0800

    Updated hand tracking sample

--- xr-plugin/com.holoi.holokit/Runtime/HoloKitHand.cs
+++ xr-plugin/com.holoi.holokit/Runtime/HoloKitHand.cs
@@ -1,8 +1,34 @@
 using System.Collections.Generic;
 using UnityEngine;
+using Holoi.HoloKit.Utils;
 
 namespace Holoi.HoloKit
 {
+    public enum LandmarkType
+    {
+        Wrist = 0,
+        Thumb0 = 1,
+        Thumb1 = 2,
+        Thumb2 = 3,
+        Thumb3 = 4,
+        Index0 = 5,
+        Index1 = 6,
+        Index2 = 7,
+        Index3 = 8,
+        Middle0 = 9,
+        Middle1 = 10,
+        Middle2 = 11,
+        Middle3 = 12,
+        Ring0 = 13,
+        Ring1 = 14,
+        Ring2 = 15,
+        Ring3 = 16,
+        Little0 = 17,
+        Little1 = 18,
+        Little2 = 19,
+        Little3 = 20
+    }
+
     public class HoloKitHand : MonoBehaviour
     {
         public List<Transform> Landmarks => _landmarks;
@@ -22,9 +48,9 @@ namespace Holoi.HoloKit
 
         public const int MAX_LANDMARK_COUNT = 21;
 
-        private const float DISAPPEAR_DELAY = 1f;
+        private const float DISAPPEAR_DELAY = 0.2f;
 
-        private void Start()
+        private void Awake()
         {
             for (int i = 0; i < MAX_LANDMARK_COUNT; i++)
             {
@@ -32,17 +58,76 @@ namespace Holoi.HoloKit
             }
         }
 
+        private void Start()
+        {
+            SetupLandmarksColor();
+
+            if (PlatformChecker.IsRuntime)
+                gameObject.SetActive(false);
+        }
+
         private void Update()
         {
             if (Time.time - _lastUpdateTime > DISAPPEAR_DELAY)
             {
-                gameObject.SetActive(false);
+                if (PlatformChecker.IsRuntime)
+                    gameObject.SetActive(false);
             }
         }
 
         public Transform GetLandmark(LandmarkType landmarkType)
         {
             return _landmarks[(int)landmarkType];
+        }
+
+        private void SetupLandmarksColor()
+        {
+            for (int i = 0; i < MAX_LANDMARK_COUNT; i++)
+            {
+                LandmarkType joint = (LandmarkType)i;
+                switch (joint)
+                {
+                    case LandmarkType.Wrist:
+                        _landmarks[i].GetComponent<MeshRenderer>().material.color = Color.red;
+                        break;
+                    case LandmarkType.Thumb0:
+                    case LandmarkType.Index0:
+                    case LandmarkType.Middle0:
+                    case LandmarkType.Ring0:
+                    case LandmarkType.Little0:
+                        _landmarks[i].GetComponent<MeshRenderer>().material.color = Color.yellow;
+                        break;
+                    case LandmarkType.Thumb1:
+                    case LandmarkType.Index1:
+                    case LandmarkType.Middle1:
+                    case LandmarkType.Ring1:
+                    case LandmarkType.Little1:
+                        _landmarks[i].GetComponent<MeshRenderer>().material.color = Color.green;
+                        break;
+                    case LandmarkType.Thumb2:
+                    case LandmarkType.Index2:
+                    case LandmarkType.Middle2:
+                    case LandmarkType.Ring2:
+                    case LandmarkType.Little2:
+                        _landmarks[i].GetComponent<MeshRenderer>().material.color = Color.cyan;
+                        break;
+                    case LandmarkType.Thumb3:
+                    case LandmarkType.Index3:
+                    case LandmarkType.Middle3:
+                    case LandmarkType.Ring3:
+                    case LandmarkType.Little3:
+                        _landmarks[i].GetComponent<MeshRenderer>().material.color = Color.blue;
+                        break;
+                }
+            }
+        }
+
+        public void SetLandmarksVisible(bool visible)
+        {
+            for (int i = 0; i < MAX_LANDMARK_COUNT; i++)
+            {
+                _landmarks[i].GetComponent<MeshRenderer>().enabled = visible;
+            }
         }
     }
 }
--- xr-plugin/com.holoi.holokit/Runtime/HoloKitHandTracker.cs
+++ xr-plugin/com.holoi.holokit/Runtime/HoloKitHandTracker.cs
@@ -1,34 +1,10 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
 using Holoi.HoloKit.NativeInterface;
 
 namespace Holoi.HoloKit
 {
-    public enum LandmarkType
-    {
-        Wrist = 0,
-        Thumb0 = 1,
-        Thumb1 = 2,
-        Thumb2 = 3,
-        Thumb3 = 4,
-        Index0 = 5,
-        Index1 = 6,
-        Index2 = 7,
-        Index3 = 8,
-        Middle0 = 9,
-        Middle1 = 10,
-        Middle2 = 11,
-        Middle3 = 12,
-        Ring0 = 13,
-        Ring1 = 14,
-        Ring2 = 15,
-        Ring3 = 16,
-        Little0 = 17,
-        Little1 = 18,
-        Little2 = 19,
-        Little3 = 20
-    }
-
     public class HoloKitHandTracker : MonoBehaviour
     {
         public static HoloKitHandTracker Instance { get { return _instance; } }
@@ -45,7 +21,7 @@ namespace Holoi.HoloKit
             set
             {
                 _enabled = value;
-                HoloKitHandTrackerNativeInterface.SetHandTrackerEnabled(_enabled);
+                SetHandTrackerEnabledInternal(_enabled);
             }
         }
 
@@ -55,7 +31,7 @@ namespace Holoi.HoloKit
             set
             {
                 _debugMode = value;
-                SetLandmarksVisible(value); 
+                SetHandsVisible(value); 
             }
         }
 
@@ -105,10 +81,9 @@ namespace Holoi.HoloKit
 
             HoloKitHandTrackerNativeInterface.OnHandPoseUpdated += OnHandPoseUpdated;
             HoloKitHandTrackerNativeInterface.RegisterHandTrackerDelegates();
-            HoloKitHandTrackerNativeInterface.SetHandTrackerEnabled(_enabled);
+            SetHandTrackerEnabledInternal(_enabled);
 
-            SetupLandmarksColor();
-            SetLandmarksVisible(_debugMode);
+            SetHandsVisible(_debugMode);
         }
 
         private void OnDestroy()
@@ -117,59 +92,26 @@ namespace Holoi.HoloKit
             HoloKitHandTrackerNativeInterface.OnHandPoseUpdated -= OnHandPoseUpdated;
         }
 
-        private void SetupLandmarksColor()
+        private void SetHandTrackerEnabledInternal(bool enabled)
         {
-            foreach (var hand in _hands)
+            if (enabled)
             {
-                for (int i = 0; i < HoloKitHand.MAX_LANDMARK_COUNT; i++)
+                var arOcclusionManager = FindObjectOfType<AROcclusionManager>();
+                if (arOcclusionManager == null || arOcclusionManager.enabled == false)
                 {
-                    LandmarkType joint = (LandmarkType)i;
-                    switch (joint)
-                    {
-                        case LandmarkType.Wrist:
-                            hand.Landmarks[i].GetComponent<MeshRenderer>().material.color = Color.red;
-                            break;
-                        case LandmarkType.Thumb0:
-                        case LandmarkType.Index0:
-                        case LandmarkType.Middle0:
-                        case LandmarkType.Ring0:
-                        case LandmarkType.Little0:
-                            hand.Landmarks[i].GetComponent<MeshRenderer>().material.color = Color.yellow;
-                            break;
-                        case LandmarkType.Thumb1:
-                        case LandmarkType.Index1:
-                        case LandmarkType.Middle1:
-                        case LandmarkType.Ring1:
-                        case LandmarkType.Little1:
-                            hand.Landmarks[i].GetComponent<MeshRenderer>().material.color = Color.green;
-                            break;
-                        case LandmarkType.Thumb2:
-                        case LandmarkType.Index2:
-                        case LandmarkType.Middle2:
-                        case LandmarkType.Ring2:
-                        case LandmarkType.Little2:
-                            hand.Landmarks[i].GetComponent<MeshRenderer>().material.color = Color.cyan;
-                            break;
-                        case LandmarkType.Thumb3:
-                        case LandmarkType.Index3:
-                        case LandmarkType.Middle3:
-                        case LandmarkType.Ring3:
-                        case LandmarkType.Little3:
-                            hand.Landmarks[i].GetComponent<MeshRenderer>().material.color = Color.blue;
-                            break;
-                    }
+                    Debug.LogError("[HoloKitSDK] You must have an AROcclusionManager before turn on hand tracking");
+                    return;
                 }
             }
+
+            HoloKitHandTrackerNativeInterface.SetHandTrackerEnabled(_enabled);
         }
 
-        private void SetLandmarksVisible(bool visible)
+        private void SetHandsVisible(bool visible)
         {
             foreach (var hand in _hands)
             {
-                for (int i = 0; i < HoloKitHand.MAX_LANDMARK_COUNT; i++)
-                {
-                    hand.Landmarks[i].GetComponent<MeshRenderer>().enabled = visible;
-                }
+                hand.SetLandmarksVisible(visible);
             }
         }
 
@@ -184,7 +126,6 @@ namespace Holoi.HoloKit
             for (int i = 0; i < HoloKitHand.MAX_LANDMARK_COUNT; i++) {
                 hand.Landmarks[i].position = new Vector3(poses[i * 3], poses[i * 3 + 1], poses[i * 3 + 2]);
             }
-
         }
     }
 }
