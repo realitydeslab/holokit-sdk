ae7c53ce039f178395079d367f55a669d7c42eb0
yuchen <yuchenz27@outlook.com>
2023-02-09 10:55:50 +0800

    added comments for HoloKitDeviceProfile script

--- xr-plugin/com.holoi.holokit/Runtime/HoloKitDeviceProfile.cs
+++ xr-plugin/com.holoi.holokit/Runtime/HoloKitDeviceProfile.cs
@@ -5,26 +5,48 @@ namespace Holoi.HoloKit
 {
     public static class HoloKitDeviceProfile
     {
+        /// <summary>
+        /// Returns true if the current device is supported by HoloKit SDK.
+        /// </summary>
+        /// <returns></returns>
         public static bool IsSupported()
         {
             return HoloKitDeviceProfileNativeInterface.IsSupported();
         }
 
+        /// <summary>
+        /// Returns true if the current device is an iPad supported by HoloKit SDK.
+        /// </summary>
+        /// <returns></returns>
         public static bool IsIpad()
         {
             return HoloKitDeviceProfileNativeInterface.IsIpad();
         }
 
+        /// <summary>
+        /// Returns true if the current device is equipped with LiDAR sensor.
+        /// </summary>
+        /// <returns></returns>
         public static bool SupportsLiDAR()
         {
             return HoloKitDeviceProfileNativeInterface.SupportsLiDAR();
         }
 
+        /// <summary>
+        /// Get the horizontal alignment marker offset in meters. Horizontal alignment marker offset
+        /// is the horizontal distance between the center of the HoloKit headset to its alignment marker.
+        /// </summary>
+        /// <returns>Horizontal alignment marker offset in meters</returns>
         public static float GetHorizontalAlignmentMarkerOffset()
         {
             return HoloKitDeviceProfileNativeInterface.GetHorizontalAlignmentMarkerOffset();
         }
 
+        /// <summary>
+        /// Get the screen dpi of the current device. DPI stands for 'dots per inch', which can be used to
+        /// convert a distance between meter-based unit and pixel-based unit.
+        /// </summary>
+        /// <returns>The screen dpi of the current device</returns>
         public static float GetScreenDpi()
         {
             return HoloKitDeviceProfileNativeInterface.GetScreenDpi();
